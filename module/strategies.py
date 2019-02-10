f9ffbbd8879d0fb43a28a7d3eb09e8a8f0125d7b
Hamdad <hamdadkhellaf1@gmail.com>
2019-02-10 14:17:20 +0100

    en cours ..

--- module/strategies.py
+++ module/strategies.py
@@ -1,136 +1,153 @@
-#coding: utf-8
 from soccersimulator import Strategy, SoccerAction, Vector2D, SoccerTeam, Simulation, show_simu, settings
 import math
-from tools import  SupState
+from tools import SupState
 class RandomStrategy(Strategy):
-   def __init__(self):
-       Strategy.__init__(self, "Random")
+    def __init__(self):
+        Strategy.__init__(self, "Random")
 
-   def compute_strategy(self, state, id_team, id_player):
-       # id_team is 1 or 2
-       # id_player starts at 0
-       return SoccerAction(Vector2D.create_random(-1.,1.),Vector2D.create_random(-1.,1.))
+    def compute_strategy(self, state, id_team, id_player):
+        # id_team is 1 or 2
+        # id_player starts at 0
+       	return SoccerAction(Vector2D.create_random(-1.,1.),Vector2D.create_random(-1.,1.))
 
 
 class AttaquantStrategy(Strategy):
-   def _init_(self):
-       """jgbuj"""
-       Strategy._init_(self,"Attaquant")
+    def _init_(self):
+        """jgbuj"""
+        Strategy._init_(self,"Attaquant")
+        
+    def compute_strategy(self,state,id_team,id_player):
+        return
+       # d=Vector2D(settings.GAME_WIDTH/2,settings.GAME_HEIGHT/2)
+       # b=state.ball.position-d
+       # k=Vector2D(1,0)
+  #      if b.dot(k):
+    #      if id_team==1
 
-   def compute_strategy(self,state,id_team,id_player):
-       return
-      # d=Vector2D(settings.GAME_WIDTH/2,settings.GAME_HEIGHT/2)
-      # b=state.ball.position-d
-      # k=Vector2D(1,0)
- #      if b.dot(k):
-   #      if id_team==1
-
-
+                                                                                                                                                  
 class FonceurStrategy(Strategy):
-   def __init__(self):
-       Strategy.__init__(self, "Fonceur")
+    def __init__(self):
+        Strategy.__init__(self, "Fonceur")
 
-   def compute_strategy(self, state, id_team, id_player):
-       # id_team is 1 or 2
-       # id_player starts at 0
-       #return SoccerAction(Vector2D.create_random(-1.,1.),Vector2D.create_random(-1.,1.))
-          ball=state.ball.position
-          player=state.player_state(id_team,id_player).position
-          #d=b-p
-          if id_team==1:
-              goal= Vector2D(settings.GAME_WIDTH,settings.GAME_HEIGHT/2)
-          else:
-              goal = Vector2D(0,settings.GAME_HEIGHT/2)
-          if player.distance(ball) < settings.PLAYER_RADIUS + settings.BALL_RADIUS:
-              shoot_alt=goal-player
-              return SoccerAction(shoot=shoot_alt)
-          else:
-              return SoccerAction(acceleration=ball-player)
-          #return SoccerAction(d,Vector2D(1,0))
+    def compute_strategy(self, state, id_team, id_player):
+        # id_team is 1 or 2
+        # id_player starts at 0
+       	#return SoccerAction(Vector2D.create_random(-1.,1.),Vector2D.create_random(-1.,1.))
+           ball=state.ball.position
+           player=state.player_state(id_team,id_player).position
+           #d=b-p
+           if id_team==1:
+               goal= Vector2D(settings.GAME_WIDTH,settings.GAME_HEIGHT/2)
+           else:
+               goal = Vector2D(0,settings.GAME_HEIGHT/2)
+           if player.distance(ball) < settings.PLAYER_RADIUS + settings.BALL_RADIUS:
+               shoot_alt=goal-player
+               if id_team==1:# ici on doit voir s'il y a un joueur a coté de la balle 
+                   shoot_alt.scale(0)
+               else:
+                   shoot_alt.scale(0.01)
+               return SoccerAction(shoot=shoot_alt)
+           else:
+               return SoccerAction(acceleration=ball-player)
+           #return SoccerAction(d,Vector2D(1,0))
 
-class TestStrategy(Strategy):
-   def __init__(self):
-       Strategy.__init__(self, "Test")
+class SoloStrategy(Strategy):
+    def __init__(self):
+        Strategy.__init__(self, "Test")
+        
+    def compute_strategy(self, state, id_team, id_player):
+        ball=state.ball.position
+        player=state.player_state(id_team,id_player).position
+        sup=SupState(state,id_team,id_player)
+           #d=b-p
+        # if sup.dist_but_adv()>settings.GAME_WIDTH//2:   
+        if player.y>2*settings.GAME_HEIGHT/3 or player.y<settings.GAME_HEIGHT/3:                  
+            if player.distance(ball) < settings.PLAYER_RADIUS + settings.BALL_RADIUS:
+                 if(sup.dist_but_adv()<settings.GAME_WIDTH/4): 
+                      return SoccerAction(shoot=sup.but_adv-sup.my_position)
+                 return (Shoot2(sup).to_goal2()+SoccerAction(acceleration=Vector2D(1,0)))
+            else:
+                  return SoccerAction(acceleration=ball-player)                 
+        else:
+            if player.distance(ball) < settings.PLAYER_RADIUS + settings.BALL_RADIUS:
+                  if(sup.dist_but_adv()<settings.GAME_WIDTH/4): 
+                      return SoccerAction(shoot=sup.but_adv-sup.my_position)
+                  else:
+                      pshoot=sup.but_adv-sup.my_position   
+                      return SoccerAction(shoot=pshoot.scale(0.02))
+            else:
+                  return SoccerAction(acceleration=ball-player)      
+          
 
-   def compute_strategy(self, state, id_team, id_player):
-          sup=SupState(state,id_team,id_player)
-          if sup.dist_but_adv()>settings.GAME_WIDTH//2:
-              if sup.can_shoot:
-                  return Shoot2(sup).to_goal2()
-              else:
-                  return SoccerAction(acceleration=sup.ball_pos-sup.pos)
-          else:
-              return Shoot2(sup).shoot2(sup.but_adv-sup.my_position)
 
 
 
-class Ssolo(Strategy):
-   def __init__(self,name):
-       Strategy.__init__(self,"SoloStrategy")
-   #def compute_strategy(self,state,idteam,idplayer):
 
 
-class Solo(Strategy):
- def __init__(self, name="soloStrategy"):
-   Strategy.__init__(self, name)
- def compute_strategy(self, state, idteam, idplayer):
-   mstate = MyState(state, idteam, idplayer)
-   adv = mstate.adv_players
-   sens = 1
-   if adv[0] == 1 :
-     sens = 1
-   else:
-     sens = -1
-   adv_pos = mstate.state.player_state(adv[0][0], adv[0][1]).position
-   me = mstate.my_position
-   if mstate.my_position.distance(mstate.but_adv) >30 :
-     if mstate.my_position.distance(mstate.state.player_state(adv[0][0], adv[0][1]).position) < 15 :
-       if mstate.my_position.distance(mstate.state.player_state(adv[0][0], adv[0][1]).position) < 3 :
-         return mstate.shoot(mstate.but_adv)
-       return mstate.drible()
-     return mstate.go_but
-   return mstate.shoot(mstate.but_adv)
+	#else:
+               #    return SoccerAction(acceleration=ball-player)
+           #return SoccerAction(d,Vector2D(1,0)) 
+         #  else:
+           #    Shoot2(sup).shoot2(sup.but_adv-sup.my_position)
+               
+               
 
+"""class Solo(Strategy):
+    def __init__(self,name):
+        Strategy.__init__(self,"SoloStrategy")
+    def compute_strategy(self,state,idteam,idplayer):        
+    
+   """ 
+        
+                          
+    
 
 class Move(object): #MOVE et SHOOT copié du cours mode tevyas zat n chikh hhh et je sais pas pk on nous a demandé de les mettre dans un fichier actions.py  or qu'on nous a demandé d'avoir juste les 3 dans notre dossier.
-   def __init__( self ,SupState):
-           self.SupState=SupState
-
-   def move(self, acceleration=None):
-           return SoccerAction(acceleration=acceleration)
-   def to_ball(self):
-           return self.move(self.SupState.ball_dir())
-
-
-
-
+    def __init__( self ,SupState):
+            self.SupState=SupState
+            
+    def move(self, acceleration=None):
+            return SoccerAction(acceleration=acceleration)
+    def to_ball(self):
+            return self.move(self.SupState.ball_dir()) 
+        
+        
+        
+        
 class Shoot2(object):
-   def __init__(self, SupState):
-       self.SupState=SupState
+    def __init__(self, SupState):
+        self.SupState=SupState
 
-   def shoot2(self, direction=None):
-       if self.SupState.dist_ball() < settings.PLAYER_RADIUS + settings.BALL_RADIUS:
-           return SoccerAction(shoot=direction)
-       else :
-           return SoccerAction(acceleration=self.SupState.ball_position-self.SupState.my_position)
+    def shoot2(self, direction=None):
+        if self.SupState.dist_ball() < settings.PLAYER_RADIUS + settings.BALL_RADIUS:
+            return SoccerAction(shoot=direction)
+        else :
+            return SoccerAction(acceleration=self.SupState.ball_position-self.SupState.my_position)
+        
+    def to_goal(self, strength=1):
+        return self.shoot2(self.SupState.goal_dir()*strength) #remplacer goal_dir par but_adv  
 
-   def to_goal(self, strength=1):
-       return self.shoot2(self.SupState.goal_dir()*strength) #remplacer goal_dir par but_adv
-
-   def to_goal2(self): #direction de la frappe
-    # return self.shoot2(direction=Vector2D(0,50))
-    #print(((self.SupState.dist_but_adv()*2**-0.5)*2))
-    return SoccerAction(shoot=Vector2D(angle=3*(math.pi/4.),norm=((self.SupState.dist_but_adv()*2**-0.5))))
-
+    def to_goal2(self): #direction de la frappe
+     # return self.shoot2(direction=Vector2D(0,50))
+     #print(((self.SupState.dist_but_adv()*2**-0.5)*2))
+     if self.Suptate.sens ==1:
+         if self.SupState.my_position.y>settings.GAME_HEIGHT/2:    
+             return SoccerAction(shoot=Vector2D(angle=1*(math.pi/4.),norm=((self.SupState.dist_but_adv()*2**-0.5))))+SoccerAction(acceleration=Vector2D(self.Supstate.sens*50,0))
+         return  SoccerAction(shoot=Vector2D(angle=7*(math.pi/4.),norm=((self.SupState.dist_but_adv()*2**-0.5))))+SoccerAction(acceleration=Vector2D(self.Suptate.sens*50,0))   
+     else:
+         if self.SupState.my_position.y>settings.GAME_HEIGHT/2:
+             return SoccerAction(shoot=Vector2D(angle=3*(math.pi/4.),norm=((self.SupState.dist_but_adv()*2**-0.5))))+SoccerAction(acceleration=Vector2D(50,0))
+         return  SoccerAction(shoot=Vector2D(angle=5*(math.pi/4.),norm=((self.SupState.dist_but_adv()*2**-0.5))))+SoccerAction(acceleration=Vector2D(50,0))   
 # Create teams
 team1 = SoccerTeam(name="Team 1")
 team2 = SoccerTeam(name="Team 2")
 
 # Add players
-team1.add("Random", RandomStrategy())  # Random strategy
+team1.add("Random",SoloStrategy())  # Random strategy
+#team1.add("Random3",FonceurStrategy()) 
 #team1.add("Fonceur1CR7", FonceurStrategy()) #Fonceur strategy
 #team2.add("Fonceur2", FonceurStrategy()) #Fonceur strategy
-team2.add("MESSI", Solo()) #Fonceur strategy
+team2.add("MESSI", SoloStrategy()) #Fonceur strategy
 print(team1.players,team2)
 #team2.add("Staatic", Strategy())   # Static strategy
 # Create a match
@@ -138,3 +155,4 @@ simu = Simulation(team1, team2)
 
 # Simulate and display the match
 show_simu(simu)
+
