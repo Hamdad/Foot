63adc6eb6a1bccf3c6b99be0138da5b0c8c1259b
Blhlyes <46899424+Blhlyes@users.noreply.github.com>
2019-03-18 16:46:05 +0100

    Update strategies.py

--- moduleH/strategies.py
+++ moduleH/strategies.py
@@ -101,8 +101,7 @@ class SoloStrategy(Strategy):
         else :
             self.i+=1
             return SoccerAction(acceleration=Vector2D(0,0)) #on reste immobile au début 
-        
-        
+
 class DefenseStrategy(Strategy):
     def __init__(self):
         Strategy.__init__(self, "Defense")
@@ -118,42 +117,8 @@ class DefenseStrategy(Strategy):
         if (sup.predict_ball().x > 5 * settings.GAME_WIDTH/8 and sup.sens==-1) or ( sup.predict_ball().x < 3* settings.GAME_WIDTH/8 and sup.sens==1):
             return SoccerAction(acceleration = sup.predict_ball() - sup.my_position)
         return SoccerAction( acceleration = centre - sup.my_position)
-    
-    
-class DefenseStrategy4(Strategy):
-    def __init__(self):
-        Strategy.__init__(self, "Defense")
-        #self.i=-50
-
-    def compute_strategy(self, state, id_team, id_player):
-        sup=SupState(state,id_team,id_player)
-        centre=sup.def_bonne_pos()
-        if sup.sens==-1: 
-            empl=Vector2D(settings.GAME_WIDTH/3,5*settings.GAME_HEIGHT/6)
-        else:
-            empl=Vector2D(4*settings.GAME_WIDTH/6,5*settings.GAME_HEIGHT/6)
-        if sup.can_shoot():
-            return SoccerAction(shoot=empl-sup.my_position , acceleration = centre - sup.my_position)
-        if sup.proche_ball():
-            return SoccerAction(acceleration = sup.predict_ball() - sup.my_position)
-        if (sup.predict_ball().x > 5 * settings.GAME_WIDTH/8 and sup.sens==-1) or ( sup.predict_ball().x < 3* settings.GAME_WIDTH/8 and sup.sens==1):
-            return SoccerAction(acceleration = sup.predict_ball() - sup.my_position)
-        return SoccerAction( acceleration = centre - sup.my_position)
         
-class DefenseStrategy2(Strategy):
-    def __init__(self):
-        Strategy.__init__(self, "Defense")
-        #self.i=-50
-
-    def compute_strategy(self, state, id_team, id_player):
-        sup=SupState(state,id_team,id_player)
-        centre=sup.def_bonne_pos(x=29*settings.GAME_WIDTH/30 if sup.sens==-1 else settings.GAME_WIDTH/30)
-        if sup.can_shoot():
-            return SoccerAction(shoot= (sup.but_adv-sup.my_position), acceleration = centre - sup.my_position)
-        if sup.proche_ball():
-            return SoccerAction(acceleration = sup.predict_ball() - sup.my_position)
-
-        return SoccerAction( acceleration = centre - sup.my_position)
+        
         
 class SoloStrategy2(Strategy):
     def __init__(self):
@@ -220,70 +185,6 @@ class SoloStrategy2(Strategy):
             return SoccerAction(acceleration= sup.predict_ball() - sup.my_position) #on reste immobile au début"""      
 
 
-class SoloStrategy3(Strategy):
-    def __init__(self):
-        Strategy.__init__(self, "Test")
-        self.i=0
-        self.first=0
-        
-    def compute_strategy(self, state, id_team, id_player):
-        sup=SupState(state,id_team,id_player)
-        if self.i == 0 :
-            if self.first>0:
-                self.first-=1
-                return sup.bien_pos2()
-            else:
-                
-                if sup.my_position.y>3*settings.GAME_HEIGHT/4 or sup.my_position.y<settings.GAME_HEIGHT/4 :    #JE SUIS DANS LES DEUX QUARTS     
-                    
-                    if sup.can_shoot() : #je peux tirer vu que proche de la balle
-                         """if(sup.dist_but_adv()<settings.GAME_WIDTH/6): #autorisation pour tirer
-                             return sup.shoot_goal()"""
-                         if (sup.my_position.x<settings.GAME_WIDTH/5 and sup.sens==-1)or(4*settings.GAME_WIDTH/5<sup.my_position.x and sup.sens==1)or sup.someone_there():# aqlagh g chwaki on dois faire le rebond ahid le corner
-                             """if (sup.my_position.y > 2*settings.GAME_HEIGHT/3) or(sup.my_position.y < settings.GAME_HEIGHT/3):
-                                 shoot=Shoot2(sup).to_goal2(-1*math.pi/4)
-                                 self.i =-2* int(sup.dist_adv_corner())/3
-                                 return shoot+SoccerAction(acceleration=Vector2D(0.2*sup.dist_adv_corner()*sup.sens))"""
-                             return sup.shoot_goal() #return SoccerAction(shoot=(sup.but_adv-sup.my_position))
-                         elif sup.someone_there() and sup.adv_closer_dist() < sup.dist_my_wall()*2 :                            
-                           shoot=Shoot2(sup).to_goal2()
-                           self.i=2*int(sup.dist_my_wall())/3
-                           #if sup.proche_ball():
-                           return shoot+SoccerAction(acceleration=Vector2D(2*sup.dist_my_wall()*sup.sens,0))#faire un rebond
-                         else :
-                           return sup.shoot_goal()
-                    else:
-                         if sup.autor_attaq():
-                             return SoccerAction(acceleration=(sup.predict_ball()-sup.my_position)*300)       #je peux pas tirer trop loin de la balle donc je vais vers la balle          
-                         else:
-                             return sup.bien_pos2()
-                else:
-                    if sup.can_shoot() : #si je suis dans les deux quarts du milieu
-                          return sup.shoot_goal()+SoccerAction(acceleration = (sup.predict_ball() - sup.my_position)*100) #je drible ac la balle 
-                    else:
-                          if sup.autor_attaq():
-                              return SoccerAction(acceleration=(sup.predict_ball()-sup.my_position)*300)   #je vais vers la balle vu que j'ai pas d'autorisation pour tirer
-                          else:
-                              return sup.bien_pos2()
-                              
-                          
-        elif self.i > 0: #a utiliser pour aller tout droit dans le cas du rebon
-            self.i -= 1
-            qte=(2*math.sqrt(2*((sup.dist_my_wall())**2)))
-            qte2=math.sqrt(qte)
-            
-            return SoccerAction(acceleration=Vector2D(sup.sens*qte2,0)) #aller tout droit aprés le rebond
-        else :
-            self.i+=1
-           # print( "le probleme est iciii")FonceurStrategy
-            qte=(2*math.sqrt(2*((sup.dist_adv_corner())**2)))
-            qte2=math.sqrt(qte)
-            
-            return SoccerAction(acceleration=Vector2D(0,sup.sens*qte2)) #aller vers la direction de la balle
-            """if sup.can_shoot():
-                return SoccerAction(shoot = (sup.but_adv-sup.my_position).norm_max(10), acceleration =  sup.predict_ball() - sup.my_position)
-            return SoccerAction(acceleration= sup.predict_ball() - sup.my_position) #on reste immobile au début"""   
-            
 
 class Move(object): #MOVE et SHOOT copié du cours mode tevyas zat n chikh hhh et je sais pas pk on nous a demandé de les mettre dans un fichier actions.py  or qu'on nous a demandé d'avoir juste les 3 dans notre dossier.
     def __init__( self ,SupState):
